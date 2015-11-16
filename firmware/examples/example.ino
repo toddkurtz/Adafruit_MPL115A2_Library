ba1a0babcf07a521577bd63e299bebdef4ee3b04
toddkurtz <toddkurtz@users.noreply.github.com>
2015-11-16 14:34:12 -0800

    Update example.ino

--- firmware/examples/example.ino
+++ firmware/examples/example.ino
@@ -1,56 +1,29 @@
-/**************************************************************************/
-/*!
-    @file     Adafruit_MPL3115A2.cpp
-    @author   K.Townsend (Adafruit Industries)
-    @license  BSD (see license.txt)
+#include "Adafruit_MPL115A2/Adafruit_MPL115A2.h"
 
-    Example for the MPL3115A2 barometric pressure sensor
+Adafruit_MPL115A2 = mpl115a2;
 
-    This is a library for the Adafruit MPL3115A2 breakout
-    ----> https://www.adafruit.com/products/1893
-
-    Adafruit invests time and resources providing this open source code,
-    please support Adafruit and open-source hardware by purchasing
-    products from Adafruit!
-
-    @section  HISTORY
-
-    v1.0.1 - Ported to Spark Core
-    v1.0 - First release
-*/
-/**************************************************************************/
-
-#include "Adafruit_MPL3115A2/Adafruit_MPL3115A2.h"
-
-// Power by connecting Vin to 3-5V, GND to GND
-// Uses I2C - connect SCL to the SCL pin, SDA to SDA pin
-// See the Wire tutorial for pinouts for each Arduino
-// http://arduino.cc/en/reference/wire
-Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
-
-void setup() {
-  Serial.begin(115200);
-  delay(5000);
+void setup(void) 
+{
+  Serial.begin(9600);
+  Serial.println("Hello!");
   
-  Serial.println("Adafruit_MPL3115A2 test!");
-  
-  while(! baro.begin()) {
-    Serial.println("Couldnt find sensor");
-    delay(1000);
-  }
+  Serial.println("Getting barometric pressure ...");
+  mpl115a2.begin();
 }
 
-void loop() {
-  float pascals = baro.getPressure();
-  // Our weather page presents pressure in Inches (Hg)
-  // Use http://www.onlineconversion.com/pressure.htm for other units
-  Serial.print(pascals/3377); Serial.println(" Inches (Hg)");
+void loop(void) 
+{
+  float pressureKPA = 0, temperatureC = 0;    
 
-  float altm = baro.getAltitude();
-  Serial.print(altm); Serial.println(" meters");
+  mpl115a2.getPT(&pressureKPA,&temperatureC);
+  Serial.print("Pressure (kPa): "); Serial.print(pressureKPA, 4); Serial.print(" kPa  ");
+  Serial.print("Temp (*C): "); Serial.print(temperatureC, 1); Serial.println(" *C both measured together");
+  
+  pressureKPA = mpl115a2.getPressure();  
+  Serial.print("Pressure (kPa): "); Serial.print(pressureKPA, 4); Serial.println(" kPa");
 
-  float tempC = baro.getTemperature();
-  Serial.print(tempC); Serial.println("*C");
-
-  delay(250);
+  temperatureC = mpl115a2.getTemperature();  
+  Serial.print("Temp (*C): "); Serial.print(temperatureC, 1); Serial.println(" *C");
+  
+  delay(1000);
 }
