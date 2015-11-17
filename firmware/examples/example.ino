9e9d4c086b6371d08fe892699c92c95451a17963
toddkurtz <toddkurtz@users.noreply.github.com>
2015-11-17 14:54:03 -0800

    Update example.ino

--- firmware/examples/example.ino
+++ firmware/examples/example.ino
@@ -1,8 +1,8 @@
-#include "Adafruit_MPL115A2/Adafruit_MPL115A2.h"
 
-Adafruit_MPL115A2 = mpl115a2;
 
-void setup(void) 
+Adafruit_MPL115A2 mpl115a2 = Adafruit_MPL115A2();
+
+void setup() 
 {
   Serial.begin(9600);
   Serial.println("Hello!");
@@ -11,9 +11,9 @@ void setup(void)
   mpl115a2.begin();
 }
 
-void loop(void) 
+void loop() 
 {
-  float pressureKPA = 0, temperatureC = 0;    
+  float pressureKPA = 0, temperatureC = 0, temperatureF = 0;    
 
   mpl115a2.getPT(&pressureKPA,&temperatureC);
   Serial.print("Pressure (kPa): "); Serial.print(pressureKPA, 4); Serial.print(" kPa  ");
@@ -23,7 +23,10 @@ void loop(void)
   Serial.print("Pressure (kPa): "); Serial.print(pressureKPA, 4); Serial.println(" kPa");
 
   temperatureC = mpl115a2.getTemperature();  
-  Serial.print("Temp (*C): "); Serial.print(temperatureC, 1); Serial.println(" *C");
+  Serial.print("Temp (*C): "); Serial.print(temperatureC, 2); Serial.println(" *C");
+  
+  temperatureF = ((temperatureC * 1.8) + 32);
+  Serial.print("Temp (*F): "); Serial.print(temperatureF, 2); Serial.println(" *F");
   
   delay(1000);
 }
